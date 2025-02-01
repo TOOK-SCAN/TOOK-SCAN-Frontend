66d6c71f78c7726d5a7494a0f24af31529ab225a
jinoo <jinoo0306@naver.com>
2025-02-01 22:57:29 +0900

    feat: GTM 환경 변수 예외 처리 및 접근성 향상 (title 속성 추가)

--- client/user/src/components/GoogleTagManager.tsx
+++ client/user/src/components/GoogleTagManager.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 export const GoogleTagManager = () => {
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID
+
+  if (!GTM_ID) {
+    console.warn('Google Tag Manager ID가 설정되지 않았습니다.')
+    return null
+  }
+
   return (
-    // <!-- Google Tag Manager (noscript) -->
     <noscript>
       <iframe
-        src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+        src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
         height="0"
         width="0"
         style={{ display: 'none', visibility: 'hidden' }}
+        title="Google Tag Manager"
       ></iframe>
     </noscript>
-    // <!-- End Google Tag Manager (noscript) -->
   )
 }
