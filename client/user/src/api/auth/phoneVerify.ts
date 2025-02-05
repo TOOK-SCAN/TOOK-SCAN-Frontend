e410cbd79e10923edafe1c9998c447464b8c95e6
Dodi029 <doyo0529@tukorea.ac.kr>
2025-02-05 14:20:18 +0900

    hotfix: 전화번호 사이에 - 안들어가는 문제 해결

--- client/user/src/api/auth/phoneVerify.ts
+++ client/user/src/api/auth/phoneVerify.ts
@@ -4,9 +4,21 @@ import { PhoneVerifyRequest, PhoneVerifyResponse } from '@/types/api/phoneAuth'
 export const patchPhoneVerify = async (
   phoneVerifyData: PhoneVerifyRequest
 ): Promise<PhoneVerifyResponse> => {
-  const response = await httpInstance.patch('auth/authentication-code', {
-    json: phoneVerifyData,
-  })
+  try {
+    console.log('요청 데이터:', phoneVerifyData) // 요청 데이터 확인
 
-  return response.json()
+    const response = await httpInstance.patch('auth/authentication-code', {
+      json: phoneVerifyData,
+    })
+
+    console.log('응답 상태 코드:', response.status)
+
+    const responseData = (await response.json()) as PhoneVerifyResponse
+    console.log('API 응답 데이터:', responseData)
+
+    return responseData
+  } catch (error) {
+    console.error('API 요청 중 에러 발생:', error)
+    throw error
+  }
 }
