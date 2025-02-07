fc0d28650d0bdac4c64e59220f9d384e920ae9bd
parkyugyeong <qkrb8019@gmai.com>
2025-02-07 11:53:48 +0900

    feat: 메인페이지 반응형 완료

--- client/user/src/app/(nonRoot)/profile/edit/layout.tsx
+++ client/user/src/app/(nonRoot)/profile/edit/layout.tsx
@@ -3,7 +3,7 @@ import type { LayoutProps } from '@/types/common'
 
 const UserInfoLayout = ({ children }: LayoutProps) => {
   return (
-    <section className="mx-auto min-w-full max-w-[37.5rem] flex-col rounded-[3rem] bg-white p-[50px] shadow-sm lg:ml-[5rem]">
+    <section className="mx-auto min-w-full max-w-[37.5rem] flex-col rounded-[3rem] bg-white px-[1rem] py-[2rem] shadow-sm lg:ml-[5rem] lg:p-[3rem]">
       {children}
     </section>
   )
