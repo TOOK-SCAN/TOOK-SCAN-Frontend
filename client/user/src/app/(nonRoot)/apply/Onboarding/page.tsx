2adc88ae6712d9d2b4431e9b08d676109e6608ca
parkyugyeong <qkrb8019@gmai.com>
2025-02-11 15:41:35 +0900

    design : html파일 퍼블리싱

---
+++ client/user/src/app/(nonRoot)/apply/Onboarding/page.tsx
@@ -0,0 +1,11 @@
+import { Banner } from '@tookscan/components'
+
+const Onboarding = () => {
+  return (
+    <div>
+      <Banner type={3}></Banner>
+    </div>
+  )
+}
+
+export default Onboarding
