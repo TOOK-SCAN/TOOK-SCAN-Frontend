3057bb65ce6e280034a4f6031e94f7f102161664
jinoo <jinoo0306@naver.com>
2025-02-15 02:02:50 +0900

    feat : 주문 내역 조회 구현

---
+++ client/user/src/app/(nonRoot)/profile/order/_components/OrderCard.tsx
@@ -0,0 +1,102 @@
+'use client'
+
+import { PaymentLabel } from '@/app/(nonRoot)/profile/order/_components/PaymentLabel'
+import type { Order } from '@/types'
+import { useModal } from '@tookscan/hooks'
+import React from 'react'
+import { orderInfo, steps } from '../_utils/order'
+
+export const OrderCard = ({ data }: { data: Order }) => {
+  const { openModal } = useModal()
+  const statusOrder = steps.map((step) => step.value)
+  const currentStepIndex = statusOrder.indexOf(data.status)
+
+  return (
+    <>
+      <div className="mx-auto mt-[12px] w-full max-w-[500px] rounded-[2rem] bg-white p-[24px] shadow md:p-[32px]">
+        {/* 상단 경로(진행단계) */}
+        <nav className="mb-4 flex items-center gap-1.5">
+          {steps.map((step, idx) => (
+            <React.Fragment key={step.value}>
+              <span
+                className={
+                  idx <= currentStepIndex
+                    ? 'font-semibold text-blue-500'
+                    : 'text-gray-500'
+                }
+              >
+                {step.label}
+              </span>
+              {idx < steps.length - 1 && (
+                <span className="text-gray-400"> &gt; </span>
+              )}
+            </React.Fragment>
+          ))}
+        </nav>
+
+        {/* 제목 */}
+        <div className="mb-4">
+          <h1 className="font-semibold btn1 md:text-xl">
+            {data.receiver_name} | {data.order_date}
+          </h1>
+        </div>
+
+        {/* 구분선 */}
+        <hr className="mb-4" />
+
+        {/* 주문 상세 정보 */}
+        <div className="mb-4 space-y-2">
+          {orderInfo(data).map(({ label, value }, index) => (
+            <div key={index} className="flex flex-row items-center">
+              <span className="mr-3 whitespace-nowrap text-black-400 caption2 md:text-sm">
+                {label}
+              </span>
+
+              <span className="truncate btn2">
+                {label === '결제 수단' ? (
+                  value ? (
+                    <PaymentLabel
+                      label="결제수단"
+                      value={
+                        data.payment_method ? data.payment_method : undefined
+                      }
+                      easyPayment={data.easy_payment_provider}
+                    />
+                  ) : (
+                    '결제 필요'
+                  )
+                ) : (
+                  value
+                )}
+              </span>
+            </div>
+          ))}
+        </div>
+
+        {/* 가격 & 버튼 영역 */}
+        <div className="flex flex-col justify-between">
+          <div className="flex flex-col items-end">
+            <div className="text-lg font-bold text-red-500">
+              {data.payment_total
+                ? `${data.payment_total.toLocaleString()}원`
+                : '최종 견적 금액 확인 중'}
+            </div>
+          </div>
+
+          {/* 버튼 그룹 (가로 정렬 유지) */}
+          <div className="flex w-full flex-row justify-center space-x-2">
+            <button className="border-1 h-[38px] w-full max-w-[13rem] rounded border border-blue-500 px-4 py-2 text-blue-500 transition hover:bg-blue-50">
+              배송조회
+            </button>
+            <button
+              className="border-1 h-[38px] w-full max-w-[13rem] rounded border border-blue-500 px-4 py-2 text-blue-500 transition hover:bg-blue-50"
+              onClick={() => openModal(<></>)}
+            >
+              주문 상세
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
